04d5a9b101fe12963e6a294db4de1e2cf2abe292
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2024-10-23 11:29:18 -0700

    Add Main page UI.

---
+++ src/MainServlet.java
@@ -0,0 +1,33 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+// declare WebServlet named MainServlet, map to URL "/api/main"
+@WebServlet(name = "MainServlet", urlPatterns = "/api/main")
+public class MainServlet extends HttpServlet{
+    private static final long serialVersionUID = 4L; // define serial UID
+
+    private DataSource dataSource; // create DataSource, registered in web
+
+    // servlet initialization
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb"); // fetch DataSource from environment, configure JDBC in application server
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+}
