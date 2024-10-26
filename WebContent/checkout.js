88b612685938fd79883638bb9f21f1eb99662b33
Kristen Bae <knbae@uci.edu>
2024-10-26 14:24:38 -0700

    added starting code for checkout page

---
+++ WebContent/checkout.js
@@ -0,0 +1,97 @@
+/**
+ * Retrieve parameter from obtained request URL, matched by parameter name
+ * @param target
+ * @returns {string|null}
+ */
+function getParameterByName(target) {
+    let url = window.location.href; // retrieve request URL
+
+    target = target.replace(/[\[\]]/g, "\\$&"); // encode target parameter name to URL encoding
+
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"), results = regex.exec(url); // use regular expression to find matched parameter value
+
+    if (!results) {
+        return null;
+    }
+
+    if (!results[2]) {
+        return '';
+    }
+
+    return decodeURIComponent(results[2].replace(/\+/g, " ")); // return decoded parameter value
+}
+
+/**
+ * Handles data returned by API, read jsonObject, and populate data into HTML elements
+ * @param resultData jsonObject
+ */
+function handleMainResult(resultData) {
+    console.log("handleMainResult: populating genre info from resultData");
+    console.log("resultData: ", resultData);
+
+    let genreInfoElement = jQuery("#genre-links");
+
+    let genreHTML = '<div class="container"><div class="row justify-content-center">';
+    for (let i = 0; i < resultData.length; i++) {
+        genreHTML += `
+            <div class="genre-column col-md-3 text-center">
+        
+                    <p>
+                        <a href="index.html?genre=${resultData[i]['genre_ids']}" class="custom-link">
+                            ${resultData[i]["movie_genre"]}
+                        </a>
+                    </p>
+        
+                
+            </div>`;
+
+        // check to end the current row and start a new one every 4 columns
+        if ((i + 1) % 4 === 0 && i + 1 < resultData.length) {
+            genreHTML += '</div><div class="row justify-content-center">';
+        }
+    }
+    genreHTML += '</div>';
+
+    genreInfoElement.append(genreHTML);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // add links to alphabetical titles
+
+    const alphaCharacters = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']; // define the alphabetical characters A-Z
+    const alphaLinksContainer = document.getElementById('alpha-links');
+
+    alphaCharacters.forEach(char => {
+        const link = document.createElement('a');
+        link.href = `index.html?prefix=${char}`;
+        link.innerText = char;
+        link.style.marginRight = '10px';
+
+        alphaLinksContainer.appendChild(link);
+    })
+
+    // add links to numerical titles
+
+    const numericCharacters = [...'0123456789', '*']; // define the numeric characters 0-9 and the special character *
+    const numericLinksContainer = document.getElementById('numeric-links');
+
+    // loop through the characters array and create <a> tags for each character
+    numericCharacters.forEach(char => {
+        const link = document.createElement('a');
+        link.href = `index.html?prefix=${char}`;
+        link.innerText = char;
+        link.style.marginRight = '10px';
+
+        numericLinksContainer.appendChild(link);
+    });
+});
+
+let genreId = getParameterByName("genre") || "";
+
+// makes HTTP GET request; upon success, uses callback function handleMainResult()
+jQuery.ajax({
+    dataType: "json", // set return data type
+    method: "GET", // set request method
+    url: "api/main?genre=" + genreId, // set request URL as mapped by MovieListServlet
+    success: (resultData) => handleMainResult(resultData) // set callback function to handle returned data from MovieListServlet
+});
---
+++ src/CheckoutServlet.java
@@ -0,0 +1,235 @@
+// derived from Project 1 API example: https://github.com/UCI-Chenli-teaching/cs122b-project1-api-example/blob/main/src/StarsServlet.java
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+// declare WebServlet named CheckoutServlet, map to URL "/api/checkout"
+@WebServlet(name = "CheckoutServlet", urlPatterns = "/api/checkout")
+public class CheckoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L; // define serial UID
+
+    private DataSource dataSource; // create DataSource, registered in web
+
+    // servlet initialization
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb"); // fetch DataSource from environment, configure JDBC in application server
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json"); // set type to "application/json" - server returns JSON object
+
+        // get parameter(s) for alphanumeric browse
+        String prefix = request.getParameter("prefix");
+        if (prefix == null || prefix.isEmpty()) {
+            prefix = "";
+        }
+
+        // get parameter(s) for genre browse
+        String genre = request.getParameter("genre");
+
+        // get parameter(s) for search
+        String title = request.getParameter("title");
+        String year = request.getParameter("year");
+        String director = request.getParameter("director");
+        String starName = request.getParameter("star");
+
+        PrintWriter out = response.getWriter(); // output stream to STDOUT
+
+        // try connection from dataSource, catch potential error(s), & close connection after usage
+        try (Connection conn = dataSource.getConnection()) {
+            String query; // define SQL query
+
+            PreparedStatement statement;
+
+            if (prefix.equals("*")) {
+                // set query for movies starting with non-alphanumeric characters
+                query = "SELECT m.id AS movie_id, m.title, m.year, m.director, r.rating, " +
+                        "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT g.name ORDER BY g.name ASC SEPARATOR ', '), ', ', 3) AS genres, " +
+                        "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT s.name ORDER BY s.name ASC SEPARATOR ', '), ', ', 3) AS stars, " +
+                        "GROUP_CONCAT(DISTINCT s.id ORDER BY s.name ASC SEPARATOR ', ') AS star_ids, " +
+                        "GROUP_CONCAT(DISTINCT g.id ORDER BY g.name ASC SEPARATOR ', ') AS genre_ids " +
+                        "FROM moviedb.movies m " +
+                        "JOIN moviedb.ratings r ON m.id = r.movieId " +
+                        "LEFT JOIN moviedb.genres_in_movies gm ON m.id = gm.movieId " +
+                        "LEFT JOIN moviedb.genres g ON gm.genreId = g.id " +
+                        "LEFT JOIN moviedb.stars_in_movies sm ON m.id = sm.movieId " +
+                        "LEFT JOIN moviedb.stars s ON sm.starId = s.id " +
+                        "WHERE m.title REGEXP '^[^a-zA-Z0-9]' " +
+                        "GROUP BY m.id, m.title, m.year, m.director, r.rating " +
+                        "ORDER BY r.rating DESC;";
+
+                statement = conn.prepareStatement(query);
+
+            } else if ((prefix.length() == 1 && Character.isLetter(prefix.charAt(0))) || (prefix.length() == 1 && Character.isDigit(prefix.charAt(0)))) {
+                // set query for movies starting with alphanumeric characters
+                query = "SELECT m.id AS movie_id, m.title, m.year, m.director, r.rating, " +
+                        "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT g.name ORDER BY g.name ASC SEPARATOR ', '), ', ', 3) AS genres, " +
+                        "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT s.name ORDER BY s.name ASC SEPARATOR ', '), ', ', 3) AS stars, " +
+                        "GROUP_CONCAT(DISTINCT s.id ORDER BY s.name ASC SEPARATOR ', ') AS star_ids, " +
+                        "GROUP_CONCAT(DISTINCT g.id ORDER BY g.name ASC SEPARATOR ', ') AS genre_ids " +
+                        "FROM moviedb.movies m " +
+                        "JOIN moviedb.ratings r ON m.id = r.movieId " +
+                        "LEFT JOIN moviedb.genres_in_movies gm ON m.id = gm.movieId " +
+                        "LEFT JOIN moviedb.genres g ON gm.genreId = g.id " +
+                        "LEFT JOIN moviedb.stars_in_movies sm ON m.id = sm.movieId " +
+                        "LEFT JOIN moviedb.stars s ON sm.starId = s.id " +
+                        "WHERE m.title LIKE ? COLLATE utf8mb4_general_ci " +
+                        "GROUP BY m.id, m.title, m.year, m.director, r.rating " +
+                        "ORDER BY r.rating DESC;";
+
+                statement = conn.prepareStatement(query);
+                statement.setString(1, prefix + "%");
+
+            } else {
+                // set query for movies according to genre
+                query = "SELECT m.id AS movie_id, m.title, m.year, m.director, r.rating, " +
+                        "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT g.name ORDER BY g.name ASC SEPARATOR ', '), ', ', 3) AS genres, " +
+                        "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT s.name ORDER BY s.name ASC SEPARATOR ', '), ', ', 3) AS stars, " +
+                        "GROUP_CONCAT(DISTINCT s.id ORDER BY s.name ASC SEPARATOR ', ') AS star_ids, " +
+                        "GROUP_CONCAT(DISTINCT g.id ORDER BY g.name ASC SEPARATOR ', ') AS genre_ids " +
+                        "FROM moviedb.movies m " +
+                        "JOIN moviedb.ratings r ON m.id = r.movieId " +
+                        "LEFT JOIN moviedb.genres_in_movies gm ON m.id = gm.movieId " +
+                        "LEFT JOIN moviedb.genres g ON gm.genreId = g.id " +
+                        "LEFT JOIN moviedb.stars_in_movies sm ON m.id = sm.movieId " +
+                        "LEFT JOIN moviedb.stars s ON sm.starId = s.id ";
+
+                List<String> conditions = new ArrayList<>();
+                List<String> params = new ArrayList<>();
+
+                if (genre != null && !genre.isEmpty()) {
+                    query += "WHERE g.id = ? ";
+
+                    query += "GROUP BY m.id, m.title, m.year, m.director, r.rating ";
+                    query += "ORDER BY r.rating DESC;";
+                }
+                else if ((title != null && !title.isEmpty()) || (year != null && !year.isEmpty()) || (director != null && !director.isEmpty()) || (starName != null && !starName.isEmpty())) {
+                    StringBuilder queryBuilder = new StringBuilder(
+                            "SELECT m.id AS movie_id, m.title, m.year, m.director, r.rating, " +
+                                    "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT g.name ORDER BY g.name ASC SEPARATOR ', '), ', ', 3) AS genres, " +
+                                    "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT s.name ORDER BY s.name ASC SEPARATOR ', '), ', ', 3) AS stars, " +
+                                    "GROUP_CONCAT(DISTINCT s.id ORDER BY s.name ASC SEPARATOR ', ') AS star_ids, " +
+                                    "GROUP_CONCAT(DISTINCT g.id ORDER BY g.name ASC SEPARATOR ', ') AS genre_ids " +
+                                    "FROM moviedb.movies m " +
+                                    "JOIN moviedb.ratings r ON m.id = r.movieId " +
+                                    "LEFT JOIN moviedb.genres_in_movies gm ON m.id = gm.movieId " +
+                                    "LEFT JOIN moviedb.genres g ON gm.genreId = g.id " +
+                                    "LEFT JOIN moviedb.stars_in_movies sm ON m.id = sm.movieId " +
+                                    "LEFT JOIN moviedb.stars s ON sm.starId = s.id "
+                    );
+
+                    // check each filter and add the relevant condition and parameter
+                    if (title != null && !title.isEmpty()) {
+                        conditions.add("m.title LIKE ?");
+                        params.add("%" + title + "%");
+                    }
+                    if (year != null && !year.isEmpty()) {
+                        conditions.add("m.year = ?");
+                        params.add(year);
+                    }
+                    if (director != null && !director.isEmpty()) {
+                        conditions.add("m.director LIKE ?");
+                        params.add("%" + director + "%");
+                    }
+                    if (starName != null && !starName.isEmpty()) {
+                        conditions.add("s.name LIKE ?");
+                        params.add("%" + starName + "%");
+                    }
+
+                    if (!conditions.isEmpty()) {
+                        queryBuilder.append(" WHERE ").append(String.join(" AND ", conditions));
+                    }
+
+                    queryBuilder.append(" GROUP BY m.id, m.title, m.year, m.director, r.rating ORDER BY r.rating DESC;");
+                    query = queryBuilder.toString();
+                }
+                else {
+                    query += "GROUP BY m.id, m.title, m.year, m.director, r.rating ";
+                    query += "ORDER BY r.rating DESC;";
+                }
+
+                statement = conn.prepareStatement(query);
+
+                if (genre != null && !genre.isEmpty()) {
+                    statement.setString(1, genre);
+                }
+                else if ((title != null && !title.isEmpty()) || (year != null && !year.isEmpty()) || (director != null && !director.isEmpty()) || (starName != null && !starName.isEmpty())) {
+                    for (int i = 0; i < params.size(); i++) {
+                        statement.setString(i + 1, params.get(i));
+                    }
+                }
+            }
+
+            ResultSet rs = statement.executeQuery(); // execute query
+
+            JsonArray jsonArray = new JsonArray(); // construct new JsonArray object
+
+            // iterate through each row of rs
+            while (rs.next()) {
+                String movie_id = rs.getString("movie_id");
+                String movie_title = rs.getString("title");
+                String movie_year = rs.getString("year");
+                String movie_director = rs.getString("director");
+                String movie_genre = rs.getString("genres");
+                String movie_star = rs.getString("stars");
+                String movie_rating = rs.getString("rating");
+                String star_ids = rs.getString("star_ids");
+                String genre_ids = rs.getString("genre_ids");
+
+                // create new JsonObject based on data retrieved from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_id", movie_id);
+                jsonObject.addProperty("movie_title", movie_title);
+                jsonObject.addProperty("movie_year", movie_year);
+                jsonObject.addProperty("movie_director", movie_director);
+                jsonObject.addProperty("movie_genre", movie_genre);
+                jsonObject.addProperty("movie_star", movie_star);
+                jsonObject.addProperty("movie_rating", movie_rating);
+                jsonObject.addProperty("star_ids", star_ids);
+                jsonObject.addProperty("genre_ids", genre_ids);
+
+                jsonArray.add(jsonObject); // add created jsonObject to jsonArray
+            }
+            rs.close(); // close rs
+            statement.close(); // close declared statement
+
+            request.getServletContext().log("getting " + jsonArray.size() + " results"); // log to localhost log
+
+            out.write(jsonArray.toString()); // write JSON string to output
+
+            response.setStatus(200); // set response status to 200 (OK)
+        } catch (Exception e) {
+            // write to output stream as JSON object
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            response.setStatus(500); // set response status to 500 (Internal Server Error)
+        } finally {
+            out.close(); // close output stream
+        }
+    }
+}
