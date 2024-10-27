044b572a87c48f4b73f6854ff47e7582b4ab26f9
Kristen Bae <knbae@uci.edu>
2024-10-26 22:13:28 -0700

    added base code for cart session

--- WebContent/checkout.js
+++ WebContent/checkout.js
@@ -1,97 +1,74 @@
+let cart = $("#cart");
+
 /**
- * Retrieve parameter from obtained request URL, matched by parameter name
- * @param target
- * @returns {string|null}
+ * Handle the data returned by IndexServlet
+ * @param resultDataString jsonObject, consists of session info
  */
-function getParameterByName(target) {
-    let url = window.location.href; // retrieve request URL
+function handleSessionData(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
 
-    target = target.replace(/[\[\]]/g, "\\$&"); // encode target parameter name to URL encoding
+    console.log("handle session response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["sessionID"]);
 
-    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"), results = regex.exec(url); // use regular expression to find matched parameter value
+    // show the session information 
+    $("#sessionID").text("Session ID: " + resultDataJson["sessionID"]);
+    $("#lastAccessTime").text("Last access time: " + resultDataJson["lastAccessTime"]);
 
-    if (!results) {
-        return null;
-    }
-
-    if (!results[2]) {
-        return '';
-    }
-
-    return decodeURIComponent(results[2].replace(/\+/g, " ")); // return decoded parameter value
+    // show cart information
+    handleCartArray(resultDataJson["previousItems"]);
 }
 
 /**
- * Handles data returned by API, read jsonObject, and populate data into HTML elements
- * @param resultData jsonObject
+ * Handle the items in item list
+ * @param resultArray jsonObject, needs to be parsed to html
  */
-function handleMainResult(resultData) {
-    console.log("handleMainResult: populating genre info from resultData");
-    console.log("resultData: ", resultData);
+function handleCartArray(resultArray) {
+    console.log(resultArray);
+    let item_list = $("#item_list");
+    // change it to html list
+    let res = "<ul>";
+    for (let i = 0; i < resultArray.length; i++) {
+        // each item will be in a bullet point
+        res += "<li>" + resultArray[i] + "</li>";
+    }
+    res += "</ul>";
 
-    let genreInfoElement = jQuery("#genre-links");
+    // clear the old array and show the new array in the frontend
+    item_list.html("");
+    item_list.append(res);
+}
 
-    let genreHTML = '<div class="container"><div class="row justify-content-center">';
-    for (let i = 0; i < resultData.length; i++) {
-        genreHTML += `
-            <div class="genre-column col-md-3 text-center">
-        
-                    <p>
-                        <a href="index.html?genre=${resultData[i]['genre_ids']}" class="custom-link">
-                            ${resultData[i]["movie_genre"]}
-                        </a>
-                    </p>
-        
-                
-            </div>`;
+/**
+ * Submit form content with POST method
+ * @param cartEvent
+ */
+function handleCartInfo(cartEvent) {
+    console.log("submit cart form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    cartEvent.preventDefault();
 
-        // check to end the current row and start a new one every 4 columns
-        if ((i + 1) % 4 === 0 && i + 1 < resultData.length) {
-            genreHTML += '</div><div class="row justify-content-center">';
+    $.ajax("api/index", {
+        method: "POST",
+        data: cart.serialize(),
+        success: resultDataString => {
+            let resultDataJson = JSON.parse(resultDataString);
+            handleCartArray(resultDataJson["previousItems"]);
         }
-    }
-    genreHTML += '</div>';
+    });
 
-    genreInfoElement.append(genreHTML);
+    // clear input form
+    cart[0].reset();
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    // add links to alphabetical titles
-
-    const alphaCharacters = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']; // define the alphabetical characters A-Z
-    const alphaLinksContainer = document.getElementById('alpha-links');
-
-    alphaCharacters.forEach(char => {
-        const link = document.createElement('a');
-        link.href = `index.html?prefix=${char}`;
-        link.innerText = char;
-        link.style.marginRight = '10px';
-
-        alphaLinksContainer.appendChild(link);
-    })
-
-    // add links to numerical titles
-
-    const numericCharacters = [...'0123456789', '*']; // define the numeric characters 0-9 and the special character *
-    const numericLinksContainer = document.getElementById('numeric-links');
-
-    // loop through the characters array and create <a> tags for each character
-    numericCharacters.forEach(char => {
-        const link = document.createElement('a');
-        link.href = `index.html?prefix=${char}`;
-        link.innerText = char;
-        link.style.marginRight = '10px';
-
-        numericLinksContainer.appendChild(link);
-    });
+$.ajax("api/index", {
+    method: "GET",
+    success: handleSessionData
 });
 
-let genreId = getParameterByName("genre") || "";
-
-// makes HTTP GET request; upon success, uses callback function handleMainResult()
-jQuery.ajax({
-    dataType: "json", // set return data type
-    method: "GET", // set request method
-    url: "api/main?genre=" + genreId, // set request URL as mapped by MovieListServlet
-    success: (resultData) => handleMainResult(resultData) // set callback function to handle returned data from MovieListServlet
-});
+// Bind the submit action of the form to a event handler function
+cart.submit(handleCartInfo);
